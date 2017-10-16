bfbe9cc3b102c1a8380f6e6964f121326f1d4694
Sakib Sami <s4kibs4mi@gmail.com>
2017-10-17 02:02:17 +0600

    logging fix & authorization added

--- app/utils/log-util.go
+++ app/utils/log-util.go
@@ -6,7 +6,8 @@ import (
 	"os"
 )
 
-var logFile, err = os.Create(viper.GetString("others.log_file"))
+var logFile *os.File
+var err error
 
 func LogD(tag string, msg interface{}) {
 	if viper.GetString("app.mode") == "debug" {
@@ -17,8 +18,16 @@ func LogD(tag string, msg interface{}) {
 }
 
 func LogF(tag string, msg interface{}) {
+	_, err := os.Stat(viper.GetString("others.log_file"))
+	if os.IsNotExist(err) {
+		logFile, err = os.Create(viper.GetString("others.log_file"))
+	}
+	if logFile == nil {
+		logFile, err = os.Open(viper.GetString("others.log_file"))
+	}
 	if err != nil {
-		logFile.WriteString(fmt.Sprintf("%s : %s", tag, msg))
+		logFile.Write([]byte(msg))
+		logFile.WriteString(fmt.Sprintf("%s : %v", tag, msg))
 	}
 }
 
